7eca95eb73ccd4166df7c12265c70d6f4c4707f3
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-10-20 15:58:26 +0200

    module test and highcharts working

--- samples/highchart.clj
+++ samples/highchart.clj
@@ -6,9 +6,7 @@
 
 ;; @@
 (ns juicy-winds
-  (:require [pinkgorilla.ui.vega :refer [vega!]]
-            [pinkgorilla.ui.highcharts :refer [highchart!]]
-            [pinkgorilla.ui.hiccup :refer [html!]]))
+  (:require [pinkgorilla.ui.highcharts :refer [highchart! module]]))
 ;; @@
 ;; =>
 ;;; ["^ ","~:type","html","~:content",["span",["^ ","~:class","clj-nil"],"nil"],"~:value","nil"]
@@ -59,7 +57,7 @@
      :series 
      [{:name "Installation" :data [439 523 577 698 931 1131 1333 1175]}
       {:name "Manufacturing" :data [249 244 292 291 390 302 381 404]}
-      {:name "Sales & Distribution" :data [117 172 165 191 285 247 321 393]}]}))      
+      {:name "Sales & Distribution" :data [117 172 165 191 285 247 321 393]}]}))
 ;; @@
 ;; =>
 ;;; ["^ ","~:type","html","~:content",["span",["^ ","~:class","clj-var"],"#'juicy-winds/highchart-spec"],"~:value","#'juicy-winds/highchart-spec"]
@@ -69,9 +67,20 @@
 (highchart! highchart-spec)
 ;; @@
 ;; =>
-;;; ["^ ","~:type","jsscript","~:content",["^ ","~:data",["^ ","~:series",[["^ ","~:name","Installation","^2",[439,523,577,698,931,1131,1333,1175]],["^ ","^4","Manufacturing","^2",[249,244,292,291,390,302,381,404]],["^ ","^4","Sales & Distribution","^2",[117,172,165,191,285,247,321,393]]],"~:legend",["^ ","~:layout","vertical","~:align","right","~:verticalAlign","top"],"~:plotOptions",["^ ","^3",["^ ","~:animation",0,"~:label",["^ ","~:connectorAllowed",false]]],"~:title",["^ ","~:text","Economic Activity"],"~:chart",["^ ","^0","line","^:",false],"~:yAxis",["^ ","~:min",0,"^=",["^ ","^>","","^7","high"]],"~:credits",["^ ","~:enabled",false],"~:subtitle",["^ ","^>","made with Love"],"~:xAxis",["^ ","~:categories",["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"]],"~:tooltip",["^ ","~:valueSuffix"," %"]],"~:module","\n  define([], function () {\n      return {\n         render: function (id_or_domel, data) {\n            var domElement = id_or_domel;\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\n               selector = '#'+ id_or_domel;\n               console.log ('module-test is rendering to selector: ' + selector);\n               domElement = document.getElementById (id); \n            } else {\n               console.log ('module-test is rendering to dom-element');\n            }\n\n            var dataJson = JSON.stringify(data)\n            console.log ('module-test data: ' + dataJson);\n\n            var p = document.createElement ('p');\n            var json = JSON.stringify (data);\n            var textnode = document.createTextNode (dataJson);  \n            //var textnode = document.createTextNode ('module-test rocks');  \n            p.appendChild (textnode);\n            domElement.appendChild (p);\n         }\n      }\n  });\n"]]
+;;; ["^ ","~:type","jsscript","~:content",["^ ","~:data",["^ ","~:series",[["^ ","~:name","Installation","^2",[439,523,577,698,931,1131,1333,1175]],["^ ","^4","Manufacturing","^2",[249,244,292,291,390,302,381,404]],["^ ","^4","Sales & Distribution","^2",[117,172,165,191,285,247,321,393]]],"~:legend",["^ ","~:layout","vertical","~:align","right","~:verticalAlign","top"],"~:plotOptions",["^ ","^3",["^ ","~:animation",0,"~:label",["^ ","~:connectorAllowed",false]]],"~:title",["^ ","~:text","Economic Activity"],"~:chart",["^ ","^0","line","^:",false],"~:yAxis",["^ ","~:min",0,"^=",["^ ","^>","","^7","high"]],"~:credits",["^ ","~:enabled",false],"~:subtitle",["^ ","^>","made with Love"],"~:xAxis",["^ ","~:categories",["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug"]],"~:tooltip",["^ ","~:valueSuffix"," %"]],"~:module","\n  define([], function () {\n      return {\n         version: 'highcharts 0.0.2',\n         render: function (id_or_domel, data) {\n            var selector_or_domel = id_or_domel;\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\n               selector_or_domel = '#'+ id_or_domel;\n               console.log ('highcharts-module is rendering to selector id: ' + selector_or_domel);\n            } else {\n               console.log ('highcharts-module is rendering to dom-element');\n            }\n            var dataJson = JSON.stringify(data)\n            console.log ('highcharts-module data: ' + dataJson);\n            require(['highcharts'], function(highcharts) {\n              new highcharts.Chart (selector_or_domel, data, {defaultStyle:true}).catch(console.warn);\n              }, function(err) {\n                console.log('Failed to load');\n            });\n         }\n      }\n  });\n"]]
+;; <=
+
+;; @@
+;; Show the Source code of the Javascript Module.
+module
+;; @@
+;; =>
+;;; ["^ ","~:type","html","~:content",["span",["^ ","~:class","clj-string"],"\"\\n  define([], function () {\\n      return {\\n         version: 'highcharts 0.0.2',\\n         render: function (id_or_domel, data) {\\n            var selector_or_domel = id_or_domel;\\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\\n               selector_or_domel = '#'+ id_or_domel;\\n               console.log ('highcharts-module is rendering to selector id: ' + selector_or_domel);\\n            } else {\\n               console.log ('highcharts-module is rendering to dom-element');\\n            }\\n            var dataJson = JSON.stringify(data)\\n            console.log ('highcharts-module data: ' + dataJson);\\n            require(['highcharts'], function(highcharts) {\\n              new highcharts.Chart (selector_or_domel, data, {defaultStyle:true}).catch(console.warn);\\n              }, function(err) {\\n                console.log('Failed to load');\\n            });\\n         }\\n      }\\n  });\\n\""],"~:value","\"\\n  define([], function () {\\n      return {\\n         version: 'highcharts 0.0.2',\\n         render: function (id_or_domel, data) {\\n            var selector_or_domel = id_or_domel;\\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\\n               selector_or_domel = '#'+ id_or_domel;\\n               console.log ('highcharts-module is rendering to selector id: ' + selector_or_domel);\\n            } else {\\n               console.log ('highcharts-module is rendering to dom-element');\\n            }\\n            var dataJson = JSON.stringify(data)\\n            console.log ('highcharts-module data: ' + dataJson);\\n            require(['highcharts'], function(highcharts) {\\n              new highcharts.Chart (selector_or_domel, data, {defaultStyle:true}).catch(console.warn);\\n              }, function(err) {\\n                console.log('Failed to load');\\n            });\\n         }\\n      }\\n  });\\n\""]
 ;; <=
 
 ;; @@
 
 ;; @@
+;; ->
+;;; 
+;; <-
---
+++ samples/module-test.clj
@@ -0,0 +1,44 @@
+;; gorilla-repl.fileformat = 2
+
+;; **
+;;; # Module-Test
+;;; 
+;;; Custom UI Modules normally include Javascript Modules.
+;;; The Test module defines a Javascript Module that renders data via javascript as json to a cell dom node.
+;; **
+
+;; @@
+(ns juicy-winds
+  (:require 
+    [pinkgorilla.ui.module-test :refer [test!]]
+    [pinkgorilla.ui.vega :refer [vega!]]
+    [pinkgorilla.ui.highcharts :refer [highchart!]]
+    [pinkgorilla.ui.hiccup :refer [html!]]))
+;; @@
+;; ->
+;;; 
+;; <-
+;; =>
+;;; ["^ ","~:type","html","~:content",["span",["^ ","~:class","clj-nil"],"nil"],"~:value","nil"]
+;; <=
+
+;; @@
+(test! {:bongo 1 :message "hello, world"})
+;; @@
+;; =>
+;;; ["^ ","~:type","jsscript","~:content",["^ ","~:data",["^ ","~:bongo",1,"~:message","hello, world"],"~:module","\n  define([], function () {\n      return {\n         version: 'module-test 0.0.2',\n         render: function (id_or_domel, data) {\n            var domElement = id_or_domel;\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\n               selector = '#'+ id_or_domel;\n               console.log ('module-test is rendering to selector: ' + selector);\n               domElement = document.getElementById (selector); \n            } else {\n               console.log ('module-test is rendering to dom-element');\n            }\n\n            var dataJson = JSON.stringify(data)\n            console.log ('module-test data: ' + dataJson);\n\n            var p = document.createElement ('p');\n            var json = JSON.stringify (data);\n            var textnode = document.createTextNode (dataJson);  \n            //var textnode = document.createTextNode ('module-test rocks');  \n            p.appendChild (textnode);\n            domElement.appendChild (p);\n         }\n      }\n  });\n"]]
+;; <=
+
+;; @@
+(html! [:h1 "hi"])
+;; @@
+;; ->
+;;; 
+;; <-
+;; =>
+;;; ["^ ","~:type","html","~:content","<h1>hi</h1>"]
+;; <=
+
+;; @@
+
+;; @@
