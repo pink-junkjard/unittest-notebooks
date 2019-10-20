c82d876b73647cd6bf4307476503a9e314076c04
awb99 <andreas.wolfgang.bauer@gmail.com>
2019-10-20 20:12:28 +0200

    vega charts working

--- samples/module-test.clj
+++ samples/module-test.clj
@@ -11,8 +11,6 @@
 (ns juicy-winds
   (:require 
     [pinkgorilla.ui.module-test :refer [test!]]
-    [pinkgorilla.ui.vega :refer [vega!]]
-    [pinkgorilla.ui.highcharts :refer [highchart!]]
     [pinkgorilla.ui.hiccup :refer [html!]]))
 ;; @@
 ;; ->
@@ -23,10 +21,10 @@
 ;; <=
 
 ;; @@
-(test! {:bongo 1 :message "hello, world"})
+(test! {:bongo 1 :message "hello, world, again"})
 ;; @@
 ;; =>
-;;; ["^ ","~:type","jsscript","~:content",["^ ","~:data",["^ ","~:bongo",1,"~:message","hello, world"],"~:module","\n  define([], function () {\n      return {\n         version: 'module-test 0.0.2',\n         render: function (id_or_domel, data) {\n            var domElement = id_or_domel;\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\n               selector = '#'+ id_or_domel;\n               console.log ('module-test is rendering to selector: ' + selector);\n               domElement = document.getElementById (selector); \n            } else {\n               console.log ('module-test is rendering to dom-element');\n            }\n\n            var dataJson = JSON.stringify(data)\n            console.log ('module-test data: ' + dataJson);\n\n            var p = document.createElement ('p');\n            var json = JSON.stringify (data);\n            var textnode = document.createTextNode (dataJson);  \n            //var textnode = document.createTextNode ('module-test rocks');  \n            p.appendChild (textnode);\n            domElement.appendChild (p);\n         }\n      }\n  });\n"]]
+;;; ["^ ","~:type","jsscript","~:content",["^ ","~:data",["^ ","~:bongo",1,"~:message","hello, world, again"],"~:module","\n  define([], function () {\n      return {\n         version: 'module-test 0.0.2',\n         render: function (id_or_domel, data) {\n            var domElement = id_or_domel;\n            if (typeof id_or_domel === 'string' || id_or_domel instanceof String) {\n               selector = '#'+ id_or_domel;\n               console.log ('module-test is rendering to selector: ' + selector);\n               domElement = document.getElementById (selector); \n            } else {\n               console.log ('module-test is rendering to dom-element');\n            }\n\n            var dataJson = JSON.stringify(data)\n            console.log ('module-test data: ' + dataJson);\n\n            var p = document.createElement ('p');\n            var json = JSON.stringify (data);\n            var textnode = document.createTextNode (dataJson);  \n            //var textnode = document.createTextNode ('module-test rocks');  \n            p.appendChild (textnode);\n            domElement.appendChild (p);\n         }\n      }\n  });\n"]]
 ;; <=
 
 ;; @@
@@ -42,3 +40,6 @@
 ;; @@
 
 ;; @@
+;; ->
+;;; 
+;; <-
